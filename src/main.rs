d11e2a26653a7bd0cd7316c9a90de0df9d8a356b
andresbermeoq <andresbermeoq1@gmail.com>
2024-02-26 10:34:03 -0500

    feat: :closed_lock_with_key: Add database configuration
    Add configuration databse with diesel and create of the schemas.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use actix_web::{
-    web, App, HttpResponse, HttpServer, Responder
-};
-mod tweets;
-mod likes;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use diesel::r2d2::{ConnectionManager, Pool};
+use diesel::PgConnection;
+use std::env;
 mod constants;
+mod likes;
+mod tweets;
 
 async fn welcome() -> impl Responder {
     HttpResponse::Ok().body("Welcome")
@@ -11,8 +12,15 @@ async fn welcome() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL don't exist");
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = Pool::builder()
+        .build(manager)
+        .expect("The connection is failed.");
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(pool.clone()))
             .route("hello", web::get().to(welcome))
             .service(tweets::get_tweets)
             .service(tweets::get_tweets_by_id)
